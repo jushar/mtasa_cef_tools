ecf29b97690993ad0175ef3a7e03351f924b8103
Jusonex <Jusonex@users.noreply.github.com>
2015-02-07 19:11:39 +0100

    Addendum to last commit

--- webui/examples/Main.lua
+++ webui/examples/Main.lua
@@ -1,14 +1,18 @@
 -- ****************************************************************************
 -- *
 -- *  PROJECT:     MTA:SA CEF utilities (https://github.com/Jusonex/mtasa_cef_tools)
--- *  FILE:        webui/tests/Main.lua
+-- *  FILE:        webui/examples/Main.lua
 -- *  PURPOSE:     Resource entry point
 -- *
 -- ****************************************************************************
 
 addEventHandler("onClientResourceStart", resourceRoot,
 	function()
-	
+		-- Do not load anything if we do not want to
+		if not get("show_examples") then	
+			return
+		end
+
 		-- Retrieve and set some basic information we need
 		local screenWidth, screenHeight = guiGetScreenSize()
 		local width, height = 640, 480
@@ -17,7 +21,7 @@ addEventHandler("onClientResourceStart", resourceRoot,
 		WebUIManager:new()
 		
 		-- Create our (local) UIs
-		--[[local window1 = WebWindow:new(Vector2(screenWidth/2-width/2-100, screenHeight/2-height/2-100), Vector2(width, height), "tests/html/ui2.html", true)
+		local window1 = WebWindow:new(Vector2(screenWidth/2-width/2-100, screenHeight/2-height/2-100), Vector2(width, height), "tests/html/ui2.html", true)
 		local window2 = WebWindow:new(Vector2(screenWidth/2-width/2, screenHeight/2-height/2), Vector2(width, height), "tests/html/ui3.html", true)
 		
 		-- Create some (remote) websites
@@ -26,7 +30,6 @@ addEventHandler("onClientResourceStart", resourceRoot,
 		-- Draw framewindows
 		local window4 = FrameWebWindow:new(Vector2(screenWidth/2-width/2+200, screenHeight/2-height/2+200), Vector2(width, height), "tests/html/ui1.html", true)
 		window4:setTitle("Window #5")
-		addEventHandler("onClientBrowserInputFocusChanged", window4:getUnderlyingBrowser(), function(state) outputChatBox("Changed: "..tostring(state)) end)
 		
 		window1:addEvent("ui2:onClose",
 			function(webwindow)
@@ -34,7 +37,6 @@ addEventHandler("onClientResourceStart", resourceRoot,
 				webwindow:destroy()
 			end
 		)
-		]]
 		
 		showCursor(true)
 	end
